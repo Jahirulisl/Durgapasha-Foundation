7d3c276d12078e8bf234c151107c5c94680b673f
Jahirul Islam <nockjahirul@gmail.com>
2025-08-19 21:36:38 +0600

    navbar footer banner and home and main and router and somebolg

---
+++ src/main.jsx
@@ -0,0 +1,20 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+
+//from react route start
+import { RouterProvider,
+} from "react-router-dom";
+
+import "./index.css";
+import { router } from './Routes/Routers';
+
+//from react route start
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+      <div className='max-w-screen-xl mx-auto'>
+        <RouterProvider router={router} />
+      </div>
+  </StrictMode>,
+)
