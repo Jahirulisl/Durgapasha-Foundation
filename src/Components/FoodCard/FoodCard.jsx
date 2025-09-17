a678eb0028cda70c54d3a59669ba9be6fafd95ed
Jahirul Islam <nockjahirul@gmail.com>
2025-09-17 18:59:09 +0600

    add some blog in FoodCard and useAxiosSecur

--- src/Components/FoodCard/FoodCard.jsx
+++ src/Components/FoodCard/FoodCard.jsx
@@ -76,7 +76,7 @@ const FoodCard = ({item}) => {
         <h2 className="card-title">{name}</h2>
         <p>{recipe}</p>
         <div className="card-actions justify-end">
-          <button onClick={hnadleAddToCart} className="btn btn-primary">Add to card</button>
+          <button onClick={() => hnadleAddToCart(item)} className="btn btn-primary">Add to card</button>
         </div>
       </div>
     </div>
